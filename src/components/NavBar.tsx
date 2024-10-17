c1bcd313d6e97b4598e28ad266cf18a86a079ad7
eijdam <133692523+eijdam@users.noreply.github.com>
2024-10-17 12:49:09 +0200

    Update NavBar.tsx

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -26,7 +26,7 @@ export default function SimpleBottomNavigation() {
     >
       <BottomNavigation
         sx={{ 
-          backgroundColor: 'white', 
+          backgroundColor: 'purple', 
           color: 'black', 
           height: 56  // Adjust the height to make it more compact if needed
         }}
@@ -62,7 +62,7 @@ export default function SimpleBottomNavigation() {
         
         {/* Prihlásenie */}
         <BottomNavigationAction
-          label="textextext"
+          label="Prihlásenie"
           icon={<LoginIcon />}
           component={Link}
           href="/auth/prihlasenie"
